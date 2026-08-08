883d2dcd2643d8423566643ee2caacbc0af45972
Kevin QIU <lysksajst@imkvq.com>
2026-08-08 13:31:38 +0900

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker compose -f docker/docker-compose.yml build
-docker save -o ~/ghostfolio+cnh.tar ghostfolio/ghostfolio:local
+docker export ghostfolio/ghostfolio:local > ~/ghostfolio+cnh.tar
